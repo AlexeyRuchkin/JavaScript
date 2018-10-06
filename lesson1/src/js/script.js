8644facff3df0584861004f49c328e94753bf4d2
Alexey <mazay_ne@bk.ru>
2018-10-06 19:26:26 +0600

    Commit 0.4

--- lesson1/src/js/script.js
+++ lesson1/src/js/script.js
@@ -7,20 +7,20 @@ let appData = {
 	budget: money,
 	timeData: time,
 	expenses: {},
-	optionalExpenses: [],
-	income: {},
+	optionalExpenses: {},
+	income: [],
 	savings: false,
-}
+};
 
-let i;
- for (i = 0; i < 1; i++) {
-
-	appData.expenses [i] = prompt ("Введите обязательную статью расходов в этом месяце");
-	appData.expenses [++i] = prompt ("Во сколько обойдется?");	
+ for (let i = 0; i < 1; i++) {
+	let a = prompt ("Введите обязательную статью расходов в этом месяце"),
+		b = prompt ("Во сколько обойдется?");	
+	
+	appData.expenses[a] = b;
 }
 
 let oneDay = money/30;
-alert("Бюджет на один день"+oneDay);
+alert("Бюджет на один день "+oneDay);
 console.log(appData);
 
 
