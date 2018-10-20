aee4aa8650e1e37876aad5ffebe8e4b6bd9ae5b5
Alexey <mazay_ne@bk.ru>
2018-10-20 18:33:43 +0600

    Commit 5.1

---
+++ lesson5/5.2/src/js/main.js
@@ -0,0 +1,23 @@
+let start = document.getElementById("start"),
+    budget = document.getElementsByClassName("budget-value"),
+    daybudget = document.getElementsByClassName("daybudget-value"),
+    level = document.getElementsByClassName("level-value"),
+    expenses = document.getElementsByClassName("expenses-value"),
+    optionalexpenses = document.getElementsByClassName("optionalexpenses-value"),
+    income = document.getElementsByClassName("income-value"),
+    monthsavings = document.getElementsByClassName("monthsavings-value"),
+    yearsavings = document.getElementsByClassName("yearsavings-value"),
+    expensesItem = document.getElementsByClassName("expenses-item"),
+    expensesBtn = document.getElementsByTagName("button")[0],
+    optionalExpensesBtn = document.getElementsByTagName("button")[1],
+    countBudgetBtn = document.getElementsByTagName("button")[2],
+    optionalExpensesItem = document.querySelectorAll(".optionalexpenses-item"),
+    incomeItem = document.querySelector(".choose-income"),
+    checkSavings = document.querySelector(".savings"),
+    sum = document.querySelector(".choose-sum"),
+    percent = document.querySelector(".choose-percent"),
+    year = document.querySelector(".year-value"),
+    month = document.querySelector(".month-value"),
+    day = document.querySelector(".day-value");
+
+
